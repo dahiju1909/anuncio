6cd628c5c3b55a9ad83d68c9f42805243486268a
dahiju1909 <0000109364574xsp@al.educacao.sp.gov.br>
2024-09-12 15:25:37 -0300

    jjjjjjj

---
+++ script.js
@@ -0,0 +1,18 @@
+const avanca = const avançar = document.querySelectorAll('.btn-proximo');
+const avançar = document.querySelectorAll('.btn-proximo');
+
+avanca.forEach()
+const  = document.querySelectorAll('.btn-proximo');
+
+avanca.forEach(button => {
+    button.addEventListener()
+
+})
+const = documentquerySelectorAll('.btn-proximo');
+
+avanca.forEach(button => {
+    button.addEventListener('click', function(){
+        const atual - document.querySelector('.ativo');
+        const proximoPasso = 'passo-' + this.getAttribute()
+     })
+})
